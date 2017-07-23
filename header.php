f0ccf6bfd935c45b31a54c9f34c846a4ce18bff2
maykilscofield <30392788+maykilscofield@users.noreply.github.com>
2017-07-23 22:17:47 +0300

    Update header.php
    elimden bu geldi :)

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <header class="container">
 	<strong class="me">SSNS</strong>
-	<a href="home" class="m" title="Home">Home</a>
-	<a href="home?p=hot" class="m" title="Hot">Hot</a>
-	<a href="home?p=week" class="m" title="Week">Week</a>
-	<a href="home?p=month" class="m" title="Month">Month</a>
+	<a href="index.php" class="m" title="Home">Home</a>
+	<a href="index.php?p=hot" class="m" title="Hot">Hot</a>
+	<a href="index.php?p=week" class="m" title="Week">Week</a>
+	<a href="index.php?p=month" class="m" title="Month">Month</a>
 	<a href="buy" class="m" title="Buy Now!">Buy Now!</a>
 </header>
 <hr>
